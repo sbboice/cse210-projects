dcad75a0b109ee65ec4fa4c270b28f675b41d605
sbboice <sethboice129@gmail.com>
2024-01-12 09:07:17 -0700

    Added functionality to the program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What grade percent did you receive? ");
+        string answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+
+        string letter = "";
+
+
+            if (grade >= 90)
+            {
+                letter = "A";
+            }
+
+            else if (grade >= 80 && grade < 90)
+            {
+                letter = "B";
+            }
+            
+            else if (grade >= 70 && grade < 90)
+            {
+                letter = "C";
+            }
+
+            else if (grade >= 60 && grade < 70)
+            {
+                letter = "D";
+            }
+
+            else if (grade < 60)
+            {
+                letter = "F";
+            }
+
+        Console.WriteLine($"You received a {letter} in the class.");
+
+            if (grade >= 70)
+            {
+                Console.WriteLine("Congratulations! You passed the class!");
+            }
+
+            else
+            {
+                Console.Write("Unfortunately, you did not pass. Keep working hard for it!");
+            }
     }
 }
