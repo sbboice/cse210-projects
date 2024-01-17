f6713186c59e4b1afec875f1a605c6743a0e3037
sbboice <sethboice129@gmail.com>
2024-01-17 12:17:00 -0700

    feat: Created a number guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int answer = randomGenerator.Next(1,11);
+
+        int guess = -1;
+
+        while (guess != answer)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (answer > guess)
+            {
+                Console.WriteLine("Too low.");
+            }
+
+            else if (answer < guess)
+            {
+                Console.WriteLine("Too high.");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+        
+  
+
+
     }
 }
