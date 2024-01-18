9e359d010fa51212298678577b68edbf21be35f0
sbboice <sethboice129@gmail.com>
2024-01-17 20:17:10 -0700

    feat: Created a program to sum and average the contents of a list.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,50 @@
-using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+       
+        List<int> numbers = new List<int>();
+
+        int input = -1;
+
+        while (input != 0)
+        {
+            Console.WriteLine("Enter Number (0 to quit):");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        }
+
+        int sum = 0;
+
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        Console.WriteLine($"The sum is: {sum}.");
+
+        float average = sum / numbers.Count;
+
+        Console.WriteLine($"The average is {average}.");
+
+        int maximum = 0;
+
+        foreach (int num in numbers)
+        {
+            if (num > maximum)
+            {
+                maximum = num;
+            }
+        }
+
+        Console.WriteLine($"The maximum value is {maximum}.");
+
     }
 }
