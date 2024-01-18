b2f42dbe84bd9da37be30735d17de1cac236c330
sbboice <sethboice129@gmail.com>
2024-01-17 20:35:04 -0700

    feat: Added functions in accordance with assignment guidelines.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+
+        int square = SquareNumber(favoriteNumber);
+
+        DisplayResult(userName, square);
     }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }  
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int SquareNumber(int num)
+    {
+        int sqrnum = num * num;
+        return sqrnum;
+    }
+
+    static void DisplayResult(string name, int number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}.");
+    }
+
 }
