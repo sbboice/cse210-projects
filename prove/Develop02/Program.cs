9e7b43b34bd2680e4c8fce8338cc1669b3f0cf8c
sbboice <sethboice129@gmail.com>
2024-01-31 13:34:43 -0700

    Created comment outline from program functions

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        //Feature 1        
+        //Display random prompt to user
+        //Ask user for input (prompt response)
+        //Save the prompt, date, and response into an internal list
+        
+        //Feature 2
+        //Display journal
+        //Iterate through and display all entries to the screen
+
+        //Feature 3
+        //Save journal to file
+        //Prompt user for a filename (journal)
+        //Save complete list of entries to specified file
+
+        //Feature 4
+        //Load journal from file
+        //Ask the user for a filename (journal)
+        //Clear locally saved entries from list
+        //Iterate through file and add each line to list
+
+        //Feature 5
+        //Provide a menu for users to choose options
+        //If statements for choices
     }
 }
