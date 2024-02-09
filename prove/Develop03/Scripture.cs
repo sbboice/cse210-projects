fed9e44db34630115b2cff27635e105b53a9738b
sbboice <sethboice129@gmail.com>
2024-02-09 11:10:52 -0700

    Built dictionary for Scripture class

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,16 +1,27 @@
+using System;
 using System.Collections.Generic;
 
 class Scripture
 {
     //Create the scripture dictionary
-    private Dictionary<string, string> scriptures = new Dictionary<string, string>{
-        {"Reference","Verse"},
-        {"",""},
-    };
-    
+    Dictionary<string, string> scripturesDict = new Dictionary<string, string>();
+
     public Scripture()
     {
-        
+        //Constructs dictionary by adding reference verse pairs
+        scripturesDict.Add("","");
     }
     
+    public void ChooseScripture()
+    {
+        //Calls Reference class to get random reference
+        Reference r = new Reference();
+        string scriptureReference = "";
+        scriptureReference = r.randomReference();
+        
+        //Searches scriptureDict based on the returned reference to see if it exists
+        //Reference is used as the key for search
+        
+
+    }
 }
